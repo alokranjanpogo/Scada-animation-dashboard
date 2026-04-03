3b6ad16e8b79ad18104b26ad1a56c6d6ff828057
alokranjanpogo <alokranjan18april@gmail.com>
2026-04-03 10:49:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ left_col, right_col = st.columns([2,1])
 def send_email_alert(message):
     sender = "alokranjan18april@gmail.com"
     password = "wpnrabqfbtkhsqpe"
-    receiver = "alokranjanjha18april@gmail.com"
+    receiver = "alok.ranjan6@tatasteel.com.com"
 
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
